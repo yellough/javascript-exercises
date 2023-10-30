df63bb057e66bc55a3437f202ea55ba856986f89
yellough <63880543+yellough@users.noreply.github.com>
2023-10-30 06:11:17 -0600

    Exercise 03 Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    for (i = 0; i < string.length; i++) {
+        output = string.charAt(i) + output;
+    }
+    return output;
 };
 
 // Do not edit below this line
