b8763e564816b7f9c11c2d580bdb61a58b114928
yellough <63880543+yellough@users.noreply.github.com>
2023-10-30 07:15:37 -0600

    Exercise 05 sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let difference;
+    let sum = 0;
+    if (int1 < 0 || int2 < 0 || typeof(int1)!="number" || typeof(int2)!="number") {
+        return "ERROR";
+    } 
+    else if (int1 < int2){
+        difference = int2 - int1;
+        for(i = 0; i <= difference; i++) {
+            newSum = int1 + i;
+            sum += newSum;
+        }
+    }
+    else {
+        difference = int1 - int2;
+        for(i = 0; i <= difference; i++) {
+            newSum = int2 + i;
+            sum += newSum;
+        }
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
