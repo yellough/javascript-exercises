6580a998af9e3b90b844d4de4fa6ec5d9b9718dd
yellough <63880543+yellough@users.noreply.github.com>
2023-10-30 07:42:41 -0600

    Exercise 07 temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+  return +parseFloat(celsiusTemp).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+  return +parseFloat(fahrenheitTemp).toFixed(1);
 };
 
 // Do not edit below this line
