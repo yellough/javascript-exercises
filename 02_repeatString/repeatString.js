b22dbe868116619862ed097495a5f937d03074c9
yellough <63880543+yellough@users.noreply.github.com>
2023-10-30 06:02:33 -0600

    Exercise 02 repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = "";
+    if (num < 0) { 
+        return "ERROR"
+    }
+    else{
+        for (i = 0; num > i; i++) {
+            output += string;
+    }     
+    return output;
+    }
 };
 
 // Do not edit below this line
