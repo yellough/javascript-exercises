47dc0f35a691c6a069e605dd3a47494993836e04
yellough <63880543+yellough@users.noreply.github.com>
2023-10-30 06:57:18 -0600

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    let outputArray = [];
+    array.forEach((item) => {
+    if (!values.includes(item)) {
+        outputArray.push(item);
+    }
+    });
+    return outputArray;
 };
 
 // Do not edit below this line
