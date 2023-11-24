5065f7b3f3ddf73c085d96faf812979e8661945b
yellough <63880543+yellough@users.noreply.github.com>
2023-11-24 06:21:39 -0700

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // make lowercase
+    str = str.toLowerCase();  
+    // convert to an array
+    // remove spaces and punctuation
+    let filtered = str.match(/[a-z|0-9]/g); 
+    // reverse the array
+    let reversed = str.match(/[a-z|0-9]/g).reverse();
+    // compare the array to the reversed array
+    // return true or false
+    return filtered.every((element, index) => element === reversed[index]);
 };
 
 // Do not edit below this line
