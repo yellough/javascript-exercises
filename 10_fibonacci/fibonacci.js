dcaa667c9297c30cd6e8e9b1bf015385cc14c916
yellough <63880543+yellough@users.noreply.github.com>
2023-11-24 06:52:22 -0700

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let f = 1;
+    let a = 0;
+    let b = 1;
+    
+    num = num*1;
+    if ( num < 0 ) {
+        return "OOPS"
+    }
+    else {
+        for ( i=2; i<=num; i++) {
+            f = a + b;
+            a = b;
+            b = f;
+        }
+        return f;
+    }
+    
 };
 
 // Do not edit below this line
